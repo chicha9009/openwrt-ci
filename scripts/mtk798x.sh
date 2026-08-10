d86e7bad6edbcc1ffeaf90dcffe59d32b445b2b8
chicha <tangtang0523@chicha.work>
2026-08-10 09:53:06 +0800

    update rax3000m config/scripts

--- scripts/mtk798x.sh
+++ scripts/mtk798x.sh
@@ -33,6 +33,10 @@ git clone --depth=1 https://github.com/chicha9009/luci-app-adguardhome.git packa
 #git clone --depth=1 https://github.com/chicha9009/adguardhome.git package/adguardhome
 git clone https://github.com/sbwml/packages_lang_golang -b 25.x feeds/packages/lang/golang
 
+echo "========== 检查 package/mtk/applications 目录内容 =========="
+ls -la "$OPENWRT_PATH/package/mtk/applications/"
+echo "=========================================================="
 
-#./scripts/feeds update -a
-#./scripts/feeds install -a
+
+./scripts/feeds update -a
+./scripts/feeds install -a
